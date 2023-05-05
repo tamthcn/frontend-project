290874af9225532a164b24ff5c19a472042f7203
tamthcn <tuanthcn1236540@gmail.com>
2023-05-05 08:25:18 +0700

    Khoi tao Project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand">Ứng dụng quản lý truyện tranh</a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                    Truyện
+                   
+                </router-link>
+            </li>
+            
+            <li class="nav-item">
+                <router-link :to="{ name: 'authorbook' }" class="nav-link">
+                    Tác giả
+                </router-link>
+            </li>
+        </div>
+    </nav>
+</template>
